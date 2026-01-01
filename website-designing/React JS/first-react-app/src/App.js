4afb00818ba27019a6c23679aab306c965ae6694
AmitRaghuvanshi1611 <thakkaramit513@gmail.com>
2026-01-01 22:28:55 +0530

    01.01.2026

---
+++ website-designing/React JS/first-react-app/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        
+        <h1>Amit Savani</h1>
+        <p>
+          Welome to My React Application!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
